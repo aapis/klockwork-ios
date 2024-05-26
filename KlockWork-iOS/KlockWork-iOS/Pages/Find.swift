ee8990ce1b0e8dec71c5cbf88a667257eb573b01
Ryan Priebe <rpriebe@me.com>
2024-05-26 10:35:01 -0600

    initial blocking out

--- KlockWork-iOS/KlockWork-iOS/Pages/Find.swift
+++ KlockWork-iOS/KlockWork-iOS/Pages/Find.swift
@@ -9,6 +9,42 @@ import SwiftUI
 
 struct Find: View {
     var body: some View {
-        Text("Coming soon!")
+        NavigationStack {
+            VStack(alignment: .leading, spacing: 0) {
+                Header()
+                ZStack(alignment: .bottomLeading) {
+//                        Tabs(job: $job, selected: $selected, date: $date)
+                    Content()
+                    LinearGradient(colors: [.black, .clear], startPoint: .bottom, endPoint: .top)
+                        .frame(height: 50)
+                        .opacity(0.1)
+                }
+
+                Editor()
+                Spacer()
+                .frame(height: 1)
+            }
+            .background(Theme.cPurple)
+        }
+    }
+}
+
+extension Find {
+    struct Header: View {
+        var body: some View {
+            Text("Header")
+        }
+    }
+
+    struct Content: View {
+        var body: some View {
+            Text("Content")
+        }
+    }
+
+    struct Editor: View {
+        var body: some View {
+            Text("Editor")
+        }
     }
 }
