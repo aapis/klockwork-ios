52d68e06d046ffe4b19bd73659fbcc168f748159
Ryan Priebe <rpriebe@me.com>
2025-07-01 13:11:31 -0600

    you can create/remove saved search terms, saved terms now list on find dashboard

--- KlockWork-iOS/KlockWork-iOS/Pages/Find.swift
+++ KlockWork-iOS/KlockWork-iOS/Pages/Find.swift
@@ -9,9 +9,10 @@ import SwiftUI
 
 struct Find: View {
     @EnvironmentObject private var state: AppState
-    @State private var text: String = ""
+    @State public var text: String = ""
     @State private var results: SearchLibrary.SearchResults?
     @State private var recentSearchTerms: [String] = [] // @TODO: store recent searches and track as another app use metric
+    @FetchRequest private var savedSearchTerms: FetchedResults<SavedSearch>
     private let page: PageConfiguration.AppPage = .find
 
     var body: some View {
@@ -19,7 +20,6 @@ struct Find: View {
             VStack(alignment: .leading, spacing: 0) {
                 Header(text: $text, recentSearchTerms: $recentSearchTerms, onSubmit: self.actionOnSubmit, page: self.page)
                 Divider().background(.white).frame(height: 1)
-
                 ZStack(alignment: .bottomLeading) {
                     if !text.isEmpty {
                         Rows(
@@ -32,9 +32,10 @@ struct Find: View {
                             List {
                                 Section("Recent Searches") {
                                     if !recentSearchTerms.isEmpty {
-                                        ForEach(recentSearchTerms, id: \.self ) { term in
+                                        ForEach(recentSearchTerms, id: \.self) { term in
                                             Button {
-                                                text = term
+                                                self.text = term
+                                                self.actionOnSubmit()
                                             } label: {
                                                 HStack(alignment: .center, spacing: 0) {
                                                     Text(term)
@@ -52,7 +53,7 @@ struct Find: View {
                                             HStack {
                                                 Text("Clear list")
                                                 Spacer()
-                                                Image(systemName: "xmark")
+                                                Image(systemName: "arrow.clockwise.square.fill")
                                             }
                                         }
                                         .listRowBackground(Color.red)
@@ -62,6 +63,22 @@ struct Find: View {
                                             .listRowBackground(Theme.textBackground)
                                             .foregroundStyle(.gray)
                                     }
+                                }
+
+                                Section("Saved Searches") {
+                                    ForEach(self.savedSearchTerms, id: \.self) { saved in
+                                        Button {
+                                            self.text = saved.term ?? "Not found"
+                                            self.actionOnSubmit()
+                                        } label: {
+                                            HStack(alignment: .center, spacing: 0) {
+                                                Text(saved.term ?? "Not found")
+                                                Spacer()
+                                                Image(systemName: "chevron.right")
+                                            }
+                                        }
+                                    }
+                                    .listRowBackground(Theme.textBackground)
                                 }
                             }
                             .background(page.primaryColour)
@@ -82,6 +99,12 @@ struct Find: View {
                 )
                 Spacer().frame(height: 1)
             }
+            .onAppear(perform: {
+                // Auto-submit if we passed in a $text value to show results right away
+                if !self.text.isEmpty {
+                    self.actionOnSubmit()
+                }
+            })
             .background(page.primaryColour)
             .onChange(of: text) {
                 if text.isEmpty {
@@ -114,6 +137,9 @@ extension Find {
                     HStack(spacing: 8) {
                         PageTitle(text: text.isEmpty ? "Find" : text.count < 10 ? "Find: \(text.prefix(10))" : "Find: \(text.prefix(10))...")
                         Spacer()
+                        if !self.text.isEmpty {
+                            SaveTermButton(text: $text)
+                        }
                         CreateEntitiesButton(page: self.page)
                     }
                 }
@@ -150,9 +176,55 @@ extension Find {
             }
         }
     }
+
+    struct SaveTermButton: View {
+        @EnvironmentObject private var state: AppState
+        @Binding public var text: String
+        @FetchRequest private var matchingSavedTerms: FetchedResults<SavedSearch>
+
+        var body: some View {
+            Button {
+                if self.matchingSavedTerms.count > 0 {
+                    CDSavedSearch(moc: self.state.moc).destroy(self.text)
+                } else {
+                    CDSavedSearch(moc: self.state.moc).create(term: self.text, created: Date())
+                }
+            } label: {
+                Image(systemName: self.matchingSavedTerms.count > 0 ? "cloud.fill" : "cloud")
+            }
+            .font(.title3)
+            .buttonStyle(.plain)
+        }
+    }
+}
+
+extension Find.SaveTermButton {
+    init(text: Binding<String>) {
+        _text = text
+        _matchingSavedTerms = CDSavedSearch.fetchMatching(term: _text.wrappedValue)
+    }
 }
 
 extension Find {
+    /// Init
+    init(text: String? = nil) {
+        if text == nil {
+            self.text = ""
+        } else {
+            self.text = text!
+        }
+
+        // 1 year in the past
+        let interval: TimeInterval = (86400*365) * -1
+
+        _savedSearchTerms = CDSavedSearch.createdBetween(
+            DateHelper.startOfMonth(for: Date().addingTimeInterval(interval)),
+            DateHelper.endOfMonth(for: Date())
+        )
+    }
+    
+    /// Fires on submit
+    /// - Returns: Void
     private func actionOnSubmit() -> Void {
         if !text.isEmpty {
             Task {
